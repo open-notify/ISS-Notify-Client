5e88da78820faf12cbd392674c51ce9c0f5e10f9
Nathan Bergey <nathan.bergey@gmail.com>
2012-11-18 00:13:44 -0800

    refactoring views

--- python/gui/views.py
+++ python/gui/views.py
@@ -3,83 +3,82 @@ import framework
 
 class MainWindow(object):
   
-  def __init__(self, controller, model):
-    self.window = framework.Window(None, "ISS Notify Update", (420,750))
-    self.model = model
-    self.control = controller
+    def __init__(self, controller, model):
+        self.window = framework.Window(None, "ISS Notify Update", (420,750))
+        self.model = model
+        self.control = controller
     
-    self.init_UI()
-    self.update_view()
+        self.init_UI()
+        self.update_view()
     
-    self.window.add_widgets()
+        self.window.add_widgets()
 
-  def init_UI(self):
+    def init_UI(self):
+        w = self.window
   
-    # Device
-    self.device_box     = self.window.add_box("Device")
-    self.device_status  = self.window.add_textinfo(self.device_box, 0, "Status:")
-    self.connect_btn    = self.window.add_button(self.device_box, 1, "Connect", self.connect_press)
+        # Device
+        self.device_box     = w.add_box("Device")
+        self.device_status  = w.add_textinfo(self.device_box,  0, "Status:")
+        self.connect_btn    = w.add_button(self.device_box,    1, "Connect", self.connect_press)
+        self.sync_btn       = w.add_button(self.device_box,    2, "Sync", self.sync_press)
     
-    # Location
-    self.location_box   = self.window.add_box("Location")
-    self.current_loc    = self.window.add_dropdown(self.location_box, 0, "My Location:", self.pick_loc)
-    self.current_lat    = self.window.add_textinfo(self.location_box, 1, "Latitude:")
-    self.current_lon    = self.window.add_textinfo(self.location_box, 2, "Longitude:")
-    self.current_alt    = self.window.add_textinfo(self.location_box, 3, "Altitude:")
-    self.manage_loc     = self.window.add_button(self.location_box,   4, "Manage Locations", self.manage_loc_press)
+        # Location
+        self.location_box   = w.add_box("Location")
+        self.current_loc    = w.add_dropdown(self.location_box, 0, "My Location:", self.pick_loc)
+        self.current_lat    = w.add_textinfo(self.location_box, 1, "Latitude:")
+        self.current_lon    = w.add_textinfo(self.location_box, 2, "Longitude:")
+        self.current_alt    = w.add_textinfo(self.location_box, 3, "Altitude:")
+        self.manage_loc     = w.add_button(self.location_box,   4, "Manage Locations", self.manage_loc_press)
     
-    # Passes
-    self.time_box       = self.window.add_box("ISS Passes")
-    self.current_time   = self.window.add_textinfo(self.time_box, 0, "Next Pass:")
-    self.get_pass_btn   = self.window.add_button(self.time_box, 1, "Get Next Pass", self.get_pass_press)
-    
-    # TEST 
-    self.test_box       = self.window.add_box("Testing")
-    self.blink_btn      = self.window.add_button(self.test_box, 0, "Blink", self.blink_press)    
-    
-  # Events
-  # ======
-  def connect_press(self, click_arg):
-    self.control.connect()
+        # Passes
+        self.time_box       = w.add_box("ISS Passes")
+        self.current_time   = w.add_textinfo(self.time_box,     0, "Next Pass:")
+        self.get_pass_btn   = w.add_button(self.time_box,       1, "Get Next Pass", self.get_pass_press)
 
-  def pick_loc(self, arg):
+
+    def update_view(self):
+
+        # Device 
+        if self.model.device_connected:
+            self.device_status.SetLabel("Connected" + self.model.device_battery_message)
+            self.connect_btn.SetLabel("...")
+            self.sync_btn.Enable()
+        else:
+            self.device_status.SetLabel("Not Connected")
+            self.sync_btn.Disable()
+ 
+        # Location
+        loc = self.model.location
+ 
+        self.current_loc.Clear()
+        for location in self.model.all_locations:
+            self.current_loc.Append(location.name)
+        self.current_loc.SetValue(loc.name)
+
+        self.current_lat.SetLabel(framework.LATITUDE_FORMAT  % loc.latitude  +u"\xb0 N")
+        self.current_lon.SetLabel(framework.LONGITUDE_FORMAT % loc.longitude +u"\xb0 E")
+        self.current_alt.SetLabel(framework.ALTITUDE_FORMAT  % loc.altitude  +u" meters")
+
+        # Passes
+        self.current_time.SetLabel(self.model.next_pass)
+
+    # Events
+    # ======
+    def connect_press(self, click_arg):
+        self.control.connect()
+
+    def sync_press(self, click_arg):
+        self.control.sync_device()
+
+    def pick_loc(self, arg):
         self.control.update_location(self.current_loc.GetValue())
 
-  def manage_loc_press(self, click_arg):
-    self.control.manage_locations()
-    
-  def blink_press(self, click_arg):
-    color = self.window.color()
-    self.control.blink(color)
+    def manage_loc_press(self, click_arg):
+        self.control.manage_locations()
 
-  def get_pass_press(self, click_arg):
-    self.control.get_next_pass()
+    def get_pass_press(self, click_arg):
+        self.control.get_next_pass()
 
-  def update_view(self):
- 
-    # Device 
-    if self.model.device_connected:
-      self.device_status.SetLabel("Connected" + self.model.device_battery_message)
-      self.connect_btn.SetLabel("...")
-      self.blink_btn.Enable()
-    else:
-      self.device_status.SetLabel("Not Connected")
-      self.blink_btn.Disable()
- 
-    # Location
-    loc = self.model.location
-    
-    self.current_loc.Clear()
-    for location in self.model.all_locations:
-        self.current_loc.Append(location.name)
-    self.current_loc.SetValue(loc.name)
-
-    self.current_lat.SetLabel(framework.LATITUDE_FORMAT  % loc.latitude  +u"\xb0 N")
-    self.current_lon.SetLabel(framework.LONGITUDE_FORMAT % loc.longitude +u"\xb0 E")
-    self.current_alt.SetLabel(framework.ALTITUDE_FORMAT  % loc.altitude  +u" meters")
-    
-    # Passes
-    self.current_time.SetLabel(self.model.next_pass)
 
 #======================================================================================
 class ManageLocations(object):
